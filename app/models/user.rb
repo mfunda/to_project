72519f0054939c4ef56381334033abd5f0aba115
Mateusz Fundakowski <mateuszfundakowski@gmail.com>
2015-11-05 00:40:17 +0100

    Another commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :products
+	has_many :products, dependent: :destroy
 	validates :name, length: {maximum: 15},
 					 presence: true
 	validates :surname, length: {maximum: 15},
--- db/migrate/20151104203733_create_products.rb
+++ db/migrate/20151104203733_create_products.rb
@@ -2,9 +2,9 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
-      t.integer :user_id
-
+	  t.references :user, index: true, foreign_key: true
       t.timestamps null: false
     end
+    add_index :products, [:user_id]
   end
 end
