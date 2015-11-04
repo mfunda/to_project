824787841cd5c8d78db9637020cdd7fab099b752
Mateusz Fundakowski <mateuszfundakowski@gmail.com>
2015-11-04 21:52:05 +0100

    Fixed models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
 	belongs_to :user
-	validates :name, length: { maximum: 20}
+	validates :name, length: { maximum: 20},
 					 presence: true	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 	has_many :products
 	validates :name, length: {maximum: 15},
 					 presence: true
-	validates :surname, length: {maximum: 15}	
+	validates :surname, length: {maximum: 15},
 					 presence: true
 end
